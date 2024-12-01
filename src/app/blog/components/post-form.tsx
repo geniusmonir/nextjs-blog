dc8941a9843a26500297b6ff714e51a72879116a
Md. Moniruzzaman <monir.jbt@gmail.com>
2024-12-01 16:28:37 +0600

    Finalize Vercel App

--- src/app/blog/components/post-form.tsx
+++ src/app/blog/components/post-form.tsx
@@ -56,14 +56,14 @@ const PostForm = () => {
         placeholder='Title'
         value={title}
         onChange={(e) => setTitle(e.target.value)}
-        className='px-4 py-2 border border-gray-300 rounded'
+        className='px-4 py-2 border border-gray-300 rounded text-blue-500'
         required
       />
       <textarea
         placeholder='Content'
         value={content}
         onChange={(e) => setContent(e.target.value)}
-        className='px-4 py-2 border border-gray-300 rounded'
+        className='px-4 py-2 border border-gray-300 rounded text-blue-500'
         rows={4}
         required
       />
@@ -72,7 +72,7 @@ const PostForm = () => {
         placeholder='Banner Image URL'
         value={bannerImg}
         onChange={(e) => setBannerImg(e.target.value)}
-        className='px-4 py-2 border border-gray-300 rounded'
+        className='px-4 py-2 border border-gray-300 rounded text-blue-500'
       />
       <label className='flex items-center gap-2'>
         <input
